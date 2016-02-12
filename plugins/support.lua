d9e8b62ce04841984f2dc6bfd0cd21e035abc3b2
alirezanile <alirezaaghajani99@gmail.com>
2016-02-12 20:55:48 +0330

    Create support.lua

---
+++ plugins/support.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return " لینک گروه پشتیبانی: https://telegram.me/joinchat/.........."
+  end
+return {
+  description = "shows enlish support", 
+  usage = "!toengsupport : Return English supports link",
+  patterns = {
+    "^support$",
+    "^/support$"
+  },
+  run = run
+}
+end
